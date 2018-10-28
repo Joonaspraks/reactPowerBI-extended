7d3ccab4e495b6310afaf78fee925885ab59e81d
joonaspraks <joonas.praks@profitsoftware.com>
2018-10-28 15:41:01 +0200

    Initial code modified for linter to accept

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "linebreak-style": "off",
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    },
+    "env": {
+        "browser": true
+    }
+};
